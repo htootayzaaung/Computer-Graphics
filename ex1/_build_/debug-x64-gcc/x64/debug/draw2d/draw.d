ce53361fb4a55137161dc9a22da79ffd4e7cf528
Htoo Aung <sc21hta@uol-pc-055423.leeds.ac.uk>
2023-10-25 12:24:31 +0100

    Uploading ex1

---
+++ ex1/_build_/debug-x64-gcc/x64/debug/draw2d/draw.d
@@ -0,0 +1,10 @@
+../_build_/debug-x64-gcc/x64/debug/draw2d/draw.o: draw.cpp draw.hpp \
+ forward.hpp ../vmlib/vec2.hpp color.hpp color.inl surface.hpp \
+ surface.inl
+draw.hpp:
+forward.hpp:
+../vmlib/vec2.hpp:
+color.hpp:
+color.inl:
+surface.hpp:
+surface.inl:
---
+++ ex1/_build_/debug-x64-gcc/x64/debug/exercise1/main.d
@@ -0,0 +1,21 @@
+../_build_/debug-x64-gcc/x64/debug/exercise1/main.o: main.cpp \
+ ../third_party/glad/include/glad.h \
+ ../third_party/glad/include/khrplatform.h \
+ ../third_party/glfw/include/GLFW/glfw3.h ../draw2d/surface.hpp \
+ ../draw2d/color.hpp ../draw2d/color.inl ../draw2d/surface.inl \
+ ../draw2d/draw.hpp ../draw2d/forward.hpp ../draw2d/../vmlib/vec2.hpp \
+ ../support/error.hpp ../support/context.hpp \
+ ../support/../draw2d/forward.hpp
+../third_party/glad/include/glad.h:
+../third_party/glad/include/khrplatform.h:
+../third_party/glfw/include/GLFW/glfw3.h:
+../draw2d/surface.hpp:
+../draw2d/color.hpp:
+../draw2d/color.inl:
+../draw2d/surface.inl:
+../draw2d/draw.hpp:
+../draw2d/forward.hpp:
+../draw2d/../vmlib/vec2.hpp:
+../support/error.hpp:
+../support/context.hpp:
+../support/../draw2d/forward.hpp:
