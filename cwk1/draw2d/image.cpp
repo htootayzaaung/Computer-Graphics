92e3d4be23a7b8bb0aa74f4cf6db649b7764c5b7
htootayzaaung <htootayzaaung.01@gmail.com>
2023-11-04 21:23:08 +0000

    Commented up to 1.6 Blitting Images...

--- cwk1/draw2d/image.cpp
+++ cwk1/draw2d/image.cpp
@@ -49,54 +49,72 @@ std::unique_ptr<ImageRGBA> load_image( char const* aPath )
 	);
 }
 
+
 /*
-void blit_masked( Surface& aSurface, ImageRGBA const& aImage, Vec2f aPosition )
-{
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	(void)aSurface;  // Avoid warnings about unused arguments until the
-	(void)aImage;    // function is properly implemented.
-	(void)aPosition;
-}
+	Image blitting is the process of transferring a block of pixels from one image (or part of an image) to another, usually to a different 
+	location in a graphics buffer.
+
+	Alpha-masking involves using an alpha channel to determine the transparency level of each pixel. In alpha-masking, pixels with lower alpha 
+	values (more transparent) can be ignored or blended with background colors, while pixels with higher alpha values (more opaque) are rendered 
+	prominently [7].
+
+	My implementation uses both of Image blitting and alpha-masking. It transfers pixels from one image to a surface (blitting), and it checks
+	the alpha value of each pixel to determine whether it should be copied over (alpha-masking).
+
+	My implementation of the blit iterates through each pixel of the source image. If a pixel's alpha value is above a certain threshold (128 in your case), 
+	the pixel is considered visible and is therefore copied to a corresponding location on the target surface. The location is calculated by adding an offset 
+	to the source image's pixel coordinates, converting them to the target surface's coordinate space.
+
+	My current implementation iterates through every pixel in the source image and checks the alpha value. It is straightforward but could be inefficient if 
+	the image has a large number of transparent pixels (alpha less than 128), as it spends time checking pixels that won't be drawn. It could be made more 
+	efficient by, for example, preprocessing the image to identify and skip regions that are entirely transparent.
+
 */
 
+// This function blits an image onto a surface with alpha masking.
 void blit_masked( Surface& aSurface, ImageRGBA const& aImage, Vec2f aPosition )
 {
+	/*
+		Here, "aSurface" is the destination where you want to blit "aImage" (which is the source image containing pixels with RGBA values). "aPosition" is 
+		the position on "aSurface" where the top-left corner of "aImage" should be placed.
+	*/
+
+	// Iterate over all the pixels in the image by height (y-coordinate)
     for (ImageRGBA::Index y = 0; y < aImage.get_height(); ++y)
     {
+		 // Iterate over all the pixels in the image by width (x-coordinate)
         for (ImageRGBA::Index x = 0; x < aImage.get_width(); ++x)
         {
+			// Retrieve the pixel color and alpha value from the image at position (x, y)
             ColorU8_sRGB_Alpha pixel = aImage.get_pixel(x, y);
-
-            if (pixel.a >= 128) // Check the alpha value.
+			
+			/*
+				If the pixel's alpha value is 128 or higher, it is visible and will be blitted to the surface. 
+				If it's less than 128, it is considered too transparent and is not drawn.
+            */
+			if (pixel.a >= 128) 
             {
-                // Convert aPosition (float) to integer coordinates.
+				/*
+					"destX" and "destY" calculate the pixel's coordinates on "aSurface" where the current pixel from "aImage" should be placed. It adds the 
+					"x" and "y" offsets (aPosition.x and aPosition.y, respectively) to the current pixel's coordinates to get the correct location on aSurface.
+				*/
+
                 int destX = static_cast<int>(aPosition.x) + x;
                 int destY = static_cast<int>(aPosition.y) + y;
 
-                // Ensure we are within the surface boundaries.
+                // Ensures that the destination coordinates (destX and destY) are within the bounds of aSurface. If the coordinates are outside aSurface, the pixel is not drawn.
                 if (destX >= 0 && destX < static_cast<int>(aSurface.get_width()) && destY >= 0 && destY < static_cast<int>(aSurface.get_height()))
                 {
+					// "convertedPixel" is created by dropping the alpha component from pixel, resulting in an RGB color that can be used on aSurface, which does not support alpha.
                     ColorU8_sRGB convertedPixel = { pixel.r, pixel.g, pixel.b };
+
+					// Set the pixel on the surface at the destination coordinates with the RGB color of the original image pixel
                     aSurface.set_pixel_srgb(destX, destY, convertedPixel);
                 }
             }
-        }
+        } 
     }
 }
-
-/*
-	Follow-up questions:
-		- What is image blitting?
-		- What is alpha-masking?
-		- Does my implementation includes both image blitting and alpha-masking?
-		- Describe the implementation of my blit.
-		- Discuss the efficiency of my implementation.
-		- Are there any implementations you could think of?
-*/
-
-
 
 namespace
 {
--- cwk1/draw2d/image.inl
+++ cwk1/draw2d/image.inl
@@ -1,32 +1,33 @@
-/*
-inline
+inline	// Inline specifier suggests that this function can be expanded at compile time where it is called to optimize performance.
 ColorU8_sRGB_Alpha ImageRGBA::get_pixel( Index aX, Index aY ) const
 {
-	assert( aX < mWidth && aY < mHeight ); // Leave this at the top of the function.
-
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	(void)aX;  // Avoid warnings about unused arguments until the function is
-	(void)aY;  // properly implemented.
-	return { 0, 0, 0, 0 }; //TODO: remove this line when you implement this
-}
-*/
-
-inline
-ColorU8_sRGB_Alpha ImageRGBA::get_pixel( Index aX, Index aY ) const
-{
+	// Ensures that the provided coordinates are within the bounds of the image dimensions.
     assert( aX < mWidth && aY < mHeight );
 
+	// Converts the 2D pixel coordinates (x, y) to a linear index in a 1D array representation of the image.
     Index linearIndex = get_linear_index(aX, aY);
 
+	// Create a variable to store the color of the pixel (RGBA format).
     ColorU8_sRGB_Alpha pixelColor;
+
+	// Access and assign the red component of the pixel from the data array.
+    // This is at the position calculated by 4 * linear index (for RGBA images).
     pixelColor.r = mData[4 * linearIndex];
+
+	// Access and assign the green component of the pixel, which is one byte after the red component.
     pixelColor.g = mData[4 * linearIndex + 1];
+
+	// Access and assign the blue component of the pixel, which is two bytes after the red component.
     pixelColor.b = mData[4 * linearIndex + 2];
+
+	// Access and assign the alpha (transparency) component of the pixel, which is three bytes after the red component.
     pixelColor.a = mData[4 * linearIndex + 3];
 
     return pixelColor;
+	/*
+		Returns the color of the pixel at the specified (x, y) position.
+		The `const` qualifier at the end of the function declaration indicates that this method doesn't modify any member variables.
+	*/
 }
 
 inline
@@ -52,21 +53,13 @@ std::uint8_t const* ImageRGBA::get_image_ptr() const noexcept
 	return mData;
 }
 
-/*
+// get_linear_index computes the position of the pixel in the data array as if the 2D image was laid out in a single line.
 inline
 ImageRGBA::Index ImageRGBA::get_linear_index( Index aX, Index aY ) const noexcept
 {
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	//TODO: your implementation goes here
-	(void)aX;  // Avoid warnings about unused arguments until the function is
-	(void)aY;  // properly implemented.
-	return 0; //TODO: remove this line when you implement this function
-}
-*/
-
-inline
-ImageRGBA::Index ImageRGBA::get_linear_index( Index aX, Index aY ) const noexcept
-{
+	/*
+		Multiply the y index by the width of the image to get the starting index of the corresponding row,
+		then add the x index to find the exact position of the pixel in the 1D array.
+    */ 
     return aY * mWidth + aX;
 }
