82e7b33b742b6749347ae4e593ac8e4221a72caf
htootayzaaung <htootayzaaung.01@gmail.com>
2023-11-07 18:40:05 +0000

    1.9 Benchmark: Blitting improved!

--- cwk1/blit-benchmark/main.cpp
+++ cwk1/blit-benchmark/main.cpp
@@ -5,28 +5,26 @@
 #include "../draw2d/image.hpp"
 #include "../draw2d/surface.hpp"
 
+// Forward declaration of the blit_masked function.
+void blit_masked(Surface& aSurface, ImageRGBA const& aImage, Vec2f aPosition);
+
 // Blit function without alpha masking using loops
 void blit_no_alpha_loops_(benchmark::State& aState, const std::string& image_path) {
     auto const fb_width = std::uint32_t(aState.range(0));
     auto const fb_height = std::uint32_t(aState.range(1));
-
     Surface surface(fb_width, fb_height);
     surface.clear();
-
     auto source = load_image(image_path.c_str());
     assert(source);
-
     for (auto _ : aState) {
-        // Blit implementation without alpha masking
         for (std::uint32_t y = 0; y < std::min(fb_height, source->get_height()); ++y) {
             for (std::uint32_t x = 0; x < std::min(fb_width, source->get_width()); ++x) {
                 ColorU8_sRGB_Alpha pixel = source->get_pixel(x, y);
                 surface.set_pixel_srgb(x, y, { pixel.r, pixel.g, pixel.b });
             }
         }
-        benchmark::ClobberMemory(); // Prevent optimization
+        benchmark::ClobberMemory();
     }
-
     aState.SetBytesProcessed(2 * std::min(fb_width, source->get_width()) * std::min(fb_height, source->get_height()) * 4 * aState.iterations());
 }
 
@@ -34,114 +32,99 @@ void blit_no_alpha_loops_(benchmark::State& aState, const std::string& image_pat
 void blit_no_alpha_memcpy_(benchmark::State& aState, const std::string& image_path) {
     auto const fb_width = std::uint32_t(aState.range(0));
     auto const fb_height = std::uint32_t(aState.range(1));
-
     Surface surface(fb_width, fb_height);
     surface.clear();
-
     auto source = load_image(image_path.c_str());
     assert(source);
-
-    // Ensure the dimensions match for blitting
     auto const blit_width = std::min(fb_width, source->get_width());
     auto const blit_height = std::min(fb_height, source->get_height());
-
     for (auto _ : aState) {
-        // Use const_cast to cast away the constness of the surface data pointer
         auto* surface_data = const_cast<std::uint8_t*>(surface.get_surface_ptr());
         auto const* image_data = source->get_image_ptr();
-
-        // Calculate the line width in bytes
         auto const line_size = blit_width * sizeof(ColorU8_sRGB);
-
         for (std::uint32_t y = 0; y < blit_height; ++y) {
-            // Compute the addresses of the source and destination lines
             auto* dest_line = surface_data + y * fb_width * sizeof(ColorU8_sRGB);
             const auto* src_line = image_data + y * source->get_width() * sizeof(ColorU8_sRGB);
-
-            // Perform the line copy
             std::memcpy(dest_line, src_line, line_size);
         }
-        benchmark::ClobberMemory(); // Prevent optimization
+        benchmark::ClobberMemory();
     }
-
     aState.SetBytesProcessed(static_cast<int64_t>(blit_width) * blit_height * 4 * aState.iterations());
 }
 
-void blit_no_alpha_loops_small_(benchmark::State& aState) {
-    blit_no_alpha_loops_(aState, "assets/impostor.png");
-}
-
-void blit_no_alpha_loops_large_(benchmark::State& aState) {
-    blit_no_alpha_loops_(aState, "assets/mortal_kombat.png");
-}
-
-void blit_no_alpha_memcpy_small_(benchmark::State& aState) {
-    blit_no_alpha_memcpy_(aState, "assets/impostor.png");
-}
-
-void blit_no_alpha_memcpy_large_(benchmark::State& aState) {
-    blit_no_alpha_memcpy_(aState, "assets/mortal_kombat.png");
-}
-
-// Existing benchmark function for blitting with alpha masking
-void default_blit_earth_(benchmark::State& aState) {
-    auto const width = std::uint32_t(aState.range(0));
-    auto const height = std::uint32_t(aState.range(1));
-
-    Surface surface(width, height);
+// Benchmark function for alpha-masked blit operation
+void benchmark_blit_masked(benchmark::State& aState, const std::string& image_path) {
+    auto const fb_width = std::uint32_t(aState.range(0));
+    auto const fb_height = std::uint32_t(aState.range(1));
+    Surface surface(fb_width, fb_height);
     surface.clear();
-
-    auto source = load_image("assets/earth.png");
+    auto source = load_image(image_path.c_str());
     assert(source);
-
     for (auto _ : aState) {
         blit_masked(surface, *source, {0.f, 0.f});
-        benchmark::ClobberMemory(); // Prevent optimization
+        benchmark::ClobberMemory();
     }
-
-
-	// The following enables the benchmarking library to print information
-	// about the memory bandwidth. The total number of bytes processed is
-	// *approximatively* two times the total number of bytes in the blit,
-	// accounding for both reading and writing. ("Approximatively" since
-	// not all pixels are written.)
-    auto const maxBlitX = std::min(width, source->get_width());
-    auto const maxBlitY = std::min(height, source->get_height());
-
-   aState.SetBytesProcessed(2 * maxBlitX * maxBlitY * 4 * aState.iterations());
+    aState.SetBytesProcessed(static_cast<int64_t>(fb_width) * fb_height * 4 * aState.iterations());
 }
 
 // Register the benchmark functions
-BENCHMARK(blit_no_alpha_memcpy_small_)
+BENCHMARK_CAPTURE(benchmark_blit_masked, impostor, "assets/impostor.png")
     ->Args({320, 240})
     ->Args({1280, 720})
     ->Args({1920, 1080})
     ->Args({7680, 4320});
 
-BENCHMARK(blit_no_alpha_memcpy_large_)
-    ->Args({320, 240})
-    ->Args({1280, 720})
-    ->Args({1920, 1080})
-    ->Args({7680, 4320});
-
-BENCHMARK(blit_no_alpha_loops_small_)
-    ->Args({320, 240})
-    ->Args({1280, 720})
-    ->Args({1920, 1080})
-    ->Args({7680, 4320});
-
-BENCHMARK(blit_no_alpha_loops_large_)
+BENCHMARK_CAPTURE(benchmark_blit_masked, mortal_kombat, "assets/mortal_kombat.png")
     ->Args({320, 240})
     ->Args({1280, 720})
     ->Args({1920, 1080})
     ->Args({7680, 4320});
 
 /*
-BENCHMARK(default_blit_earth_)
+BENCHMARK_CAPTURE(benchmark_blit_masked, earth, "assets/earth.png")
     ->Args({320, 240})
     ->Args({1280, 720})
     ->Args({1920, 1080})
     ->Args({7680, 4320});
 */
 
+BENCHMARK_CAPTURE(blit_no_alpha_loops_, impostor, "assets/impostor.png")
+    ->Args({320, 240})
+    ->Args({1280, 720})
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
+
+BENCHMARK_CAPTURE(blit_no_alpha_loops_, mortal_kombat, "assets/mortal_kombat.png")
+    ->Args({320, 240})
+    ->Args({1280, 720})
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
+
+/*
+BENCHMARK_CAPTURE(blit_no_alpha_loops_, mortal_kombat, "assets/earth.png")
+    ->Args({320, 240})
+    ->Args({1280, 720})
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
+*/
+
+BENCHMARK_CAPTURE(blit_no_alpha_memcpy_, impostor, "assets/impostor.png")
+    ->Args({320, 240})
+    ->Args({1280, 720})
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
+
+BENCHMARK_CAPTURE(blit_no_alpha_memcpy_, mortal_kombat, "assets/mortal_kombat.png")
+    ->Args({320, 240})
+    ->Args({1280, 720})
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
+
+/*
+BENCHMARK_CAPTURE(blit_no_alpha_memcpy_, mortal_kombat, "assets/earth.png")
+    ->Args({320, 240})
+    ->Args({1280, 720})
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
+*/
 BENCHMARK_MAIN();
--- cwk1/lines-benchmark/main.cpp
+++ cwk1/lines-benchmark/main.cpp
@@ -1,36 +1,67 @@
 #include <benchmark/benchmark.h>
-
+#include <cmath> // For std::round and std::abs
 #include "../draw2d/draw.hpp"
 #include "../draw2d/surface.hpp"
 
-namespace
-{
-	// This is a placeholder. Replace this with yor own code. Refer to
-	// blit-benchmark/main.cpp for a more complete example. 
-	void placeholder_( benchmark::State& aState )
-	{
-		auto const width = std::uint32_t(aState.range(0));
-		auto const height = std::uint32_t(aState.range(1));
-
-		Surface surface( width, height );
-		surface.clear();
-
-		for( auto _ : aState )
-		{
-			// Placeholder that just does something:
-			surface.clear(); // PLACEHOLDER! EXCLUDE FROM REAL BENCHMARKS!
-
-			// ClobberMemory() ensures that the compiler won't optimize away
-			// our blit operation. (Unlikely, but technically poossible.)
-			benchmark::ClobberMemory(); 
-		}
-	}
+// DDA line drawing function
+void draw_line_dda(Surface& surface, int x1, int y1, int x2, int y2, ColorU8_sRGB color) {
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int steps = std::max(std::abs(dx), std::abs(dy));
+    float xInc = dx / static_cast<float>(steps);
+    float yInc = dy / static_cast<float>(steps);
+    float x = x1;
+    float y = y1;
+    for (int i = 0; i <= steps; ++i) {
+        surface.set_pixel_srgb(static_cast<int>(std::round(x)), static_cast<int>(std::round(y)), color);
+        x += xInc;
+        y += yInc;
+    }
 }
 
-BENCHMARK( placeholder_ )
-	->Args( { 1920, 1080 } )
-	->Args( { 7680, 4320 } )
-;
+// Benchmark function for DDA line drawing
+void line_drawing_dda_benchmark(benchmark::State& aState) {
+    auto const width = std::uint32_t(aState.range(0));
+    auto const height = std::uint32_t(aState.range(1));
 
+    Surface surface(width, height);
+    ColorU8_sRGB color = {255, 255, 255}; // White color for the line
+
+    for (auto _ : aState) {
+        surface.clear();
+        draw_line_dda(surface, 0, 0, width - 1, height - 1, color);
+        benchmark::ClobberMemory();
+    }
+
+    aState.SetBytesProcessed(static_cast<int64_t>(width + height - 1) * aState.iterations());
+}
+
+// Assuming the Bresenham's line drawing function is named draw_line_solid and declared in draw.hpp
+void line_drawing_bresenham_benchmark(benchmark::State& aState) {
+    auto const width = std::uint32_t(aState.range(0));
+    auto const height = std::uint32_t(aState.range(1));
+
+    Surface surface(width, height);
+    ColorU8_sRGB color = {255, 255, 255}; // White color for the line
+    Vec2f begin{0, 0};
+    Vec2f end{static_cast<float>(width - 1), static_cast<float>(height - 1)};
+
+    for (auto _ : aState) {
+        surface.clear();
+        draw_line_solid(surface, begin, end, color); // Call your Bresenham's line drawing function here
+        benchmark::ClobberMemory();
+    }
+
+    aState.SetBytesProcessed(static_cast<int64_t>(width + height - 1) * aState.iterations());
+}
+
+// Register the benchmark functions
+BENCHMARK(line_drawing_dda_benchmark)
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
+
+BENCHMARK(line_drawing_bresenham_benchmark)
+    ->Args({1920, 1080})
+    ->Args({7680, 4320});
 
 BENCHMARK_MAIN();
