61ed2548740f8ca76ddb39015c5e8436a17b782a
htootayzaaung <htootayzaaung.01@gmail.com>
2023-12-03 22:44:32 +0000

    Commencing Exercise 5

---
+++ Exercises/ex5/exercise5/cylinder.cpp
@@ -0,0 +1,9 @@
+#include "cylinder.hpp"
+
+#include "../vmlib/mat33.hpp"
+
+SimpleMeshData make_cylinder( bool aCapped, std::size_t aSubdivs, Vec3f aColor, Mat44f aPreTransform )
+{
+	//TODO: implement me
+	return {};
+}
